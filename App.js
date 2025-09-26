fcdcdd0d3f529bd4e680ccd4701664632291fd37
Pedro Yuji Hamada <pedro.yuji.hamada@gmail.com>
2025-09-26 10:42:22 -0300

    finalizar prova

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>2o Ano B</Text>
+      <Text>2025</Text>
+      <Text>Pedro Yuji Hamada</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
